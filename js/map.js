70508ff2a67ff376f9dbc1df0d020621eea4a065
dmitriyd-web <yadimon1989@gmail.com>
2024-03-28 17:52:59 +0300

    first commit

---
+++ js/map.js
@@ -0,0 +1,36 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.761101, 37.628845],
+            zoom: 16,
+			controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemarkWithContent = new ymaps.Placemark([55.761101, 37.629300], {
+            hintContent: 'Собственный значок метки с контентом',
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/map-icon.png',
+            // Размеры метки.
+            iconImageSize: [60, 68],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+    myMap.geoObjects
+        .add(myPlacemarkWithContent);		
+});
